1bc8175db242289b08811185cf720f70aa09d6a6
talza <talzamir92@gmail.com>
2017-08-08 20:24:38 +0300

    Battleship fields added.

--- src/logic/Battleship.java
+++ src/logic/Battleship.java
@@ -1,5 +1,14 @@
 package logic;
 
+import logic.enums.BattleshipType;
+
 public class Battleship {
+
+    private String id;
+    private BattleshipType type;
+    private int size;
+    private int score;
+    private boolean isAlive;
+
 
 }
