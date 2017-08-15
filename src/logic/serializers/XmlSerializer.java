e161dbd374eecda48d6bf31148904f861c8d45cd
talza <talzamir92@gmail.com>
2017-08-15 20:05:45 +0300

    Load XML full path fixed.

--- src/logic/serializers/XmlSerializer.java
+++ src/logic/serializers/XmlSerializer.java
@@ -7,6 +7,7 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
+import java.io.File;
 
 public class XmlSerializer implements ISerializer {
 
@@ -14,12 +15,13 @@ public class XmlSerializer implements ISerializer {
     private final BattleShipGameType battleShipGameType;
 
     public XmlSerializer(String xmlPath) throws JAXBException {
+        File file = new File(xmlPath);
         JAXBContext jaxbContext;
         JAXBElement unmarshalledObject;
         Unmarshaller unmarshaller;
         jaxbContext = JAXBContext.newInstance(CONTEXT_PATH);
         unmarshaller = jaxbContext.createUnmarshaller();
-        unmarshalledObject = (JAXBElement) unmarshaller.unmarshal(ClassLoader.getSystemResourceAsStream(xmlPath));
+        unmarshalledObject = (JAXBElement) unmarshaller.unmarshal(file);
         assert unmarshalledObject != null;
         battleShipGameType = (BattleShipGameType) unmarshalledObject.getValue();
     }
